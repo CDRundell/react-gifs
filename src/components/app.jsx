6d80f3e442fd22509e4faee2cb90a6eac84bfc3a
Chris Rundell <cdrundell@icloud.com>
2022-04-07 17:45:11 +0100

    completed up to lecture

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,50 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "ue0RpimpEN2k3gGJcO"
+    };
+
+    this.search("disney");
+  }
+
+  search = (query) => {
+    giphy("h5bMAcEFXWItwLdyz524SCmp4f27ipTM").search({
+      q: query,
+      rating: 'g',
+      limit: 10 }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render() {
+    // const gifs = [
+    //   { id: "ue0RpimpEN2k3gGJcO" },
+    //   { id: "ue0RpimpEN2k3gGJcO" }
+    // ];
+
     return (
     <div>
       <div className="left-scene">
-        <SearchBar />
+        <SearchBar searchFunction={this.search} />
         <div className="selected-gif">
-        <Gif />
+            <Gif id={this.state.selectedGifId} />
         </div>
       </div>
-      <div className="right-scene"></div>
+      <div className="right-scene">
+        <GifList gifs={this.state.gifs} />
+      </div>
     </div>
     )}
 }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render () {
+    const src = `https://media1.giphy.com/media/${this.props.id}/giphy.gif?cid=ecf05e47x48ub5av6utdlgxi2duseqo24khsblyne5orouns&rid=giphy.gif&ct=g`;
     return (
-      <img src="https://media1.giphy.com/media/ue0RpimpEN2k3gGJcO/giphy.gif?cid=ecf05e47x48ub5av6utdlgxi2duseqo24khsblyne5orouns&rid=giphy.gif&ct=g"></img>
+      <img src = {src} className="gif"></img>
     );
   }
 }
