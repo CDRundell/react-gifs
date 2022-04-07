21b4392c90d2be5ddeb813357e182b3f55de29da
Chris Rundell <cdrundell@icloud.com>
2022-04-07 16:40:29 +0100

    made search bar and gif components

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render () {
+    return (
+      <img src="https://media1.giphy.com/media/ue0RpimpEN2k3gGJcO/giphy.gif?cid=ecf05e47x48ub5av6utdlgxi2duseqo24khsblyne5orouns&rid=giphy.gif&ct=g"></img>
+    );
+  }
+}
+
+export default Gif;
